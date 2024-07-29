aa5cf965b29eb221ac61ad5c7970d48358342594
TashiPatsa <tashitseten2004@gmail.com>
2024-07-29 19:15:09 -0400

    created routes folder and orgnaized code

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import { PORT, mongoDBURL} from "./config.js";
 import mongoose from 'mongoose';
 import { Book } from './models/bookModel.js';
+import booksRoute from './routes/booksRoute.js';
 
 const app = express();
 
@@ -12,47 +13,7 @@ app.get('/', (request, response) => {
    return response.status(234).send('BOOKSTORE SOON TO BE')
 });
 
-app.post('/books', async (request,response) => {
-    try{
-        if(
-            !request.body.title ||
-            !request.body.author ||
-            !request.body.publishYear
-        ) {
-            return response.status(400).send({
-                message: 'Send all required fields:title, author, publishYear',
-            });
-        }
-        const newBook = {
-            title: request.body.title,
-            author: request.body.author,
-            publishYear: request.body.publishYear,
-        };
-
-        const book = await Book.create(newBook);
-
-        return response.status(201).send(book);
-    } catch (error){
-        console.log(error.message);
-        response.status(500).send({message: error.message});
-    }
-});
-
-//Route to get all books from database
-app.get('/books', async (request, response) => {
-    try{
-        const books = await Book.find({});
-        return response.status(200).json(
-            {
-                count: books.length,
-                data: books
-            }
-        );
-    }catch(error){
-        console.log(error.message);
-        response.status(500).send({message:error});
-    }
-});
+app.use('/books', booksRoute);
 
 mongoose
     .connect(mongoDBURL)
