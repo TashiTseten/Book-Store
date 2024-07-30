b366135af043ba074bc35fc1813bb2994cbbbd31
TashiPatsa <tashitseten2004@gmail.com>
2024-07-29 20:07:52 -0400

    working on frontend

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,19 @@ import { PORT, mongoDBURL} from "./config.js";
 import mongoose from 'mongoose';
 import { Book } from './models/bookModel.js';
 import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
 
 const app = express();
 
+//Middleware for parsing request body
 app.use(express.json());
+
+//Middleware for handling CORS POLICY
+app.use(cors({
+    origin:'http://localhost:3000',
+    methods:['GET','POST','PUT','DELETE'],
+    allowedHeaders:['Content-Type'],
+}));
 
 app.get('/', (request, response) => {
    console.log(request)
